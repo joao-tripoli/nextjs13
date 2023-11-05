8416c0962c4673c6bad3bc8b0d4fe3fbc86b7dca
João Matheus Tripoli <joao.tripoli.dev@gmail.com>
2023-11-05 17:31:50 -0300

    End of chapter 13

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import { ReactNode } from 'react';
+
+export const metadata = {
+  title: 'Next.js 13 with Clerk',
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
